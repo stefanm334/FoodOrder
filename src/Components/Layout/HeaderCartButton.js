39752d18360da70aa58603113db30f87b8cfb217
stefanm334 <stefan_m32@hotmail.com>
2021-05-15 10:06:21 +0200

    adding features

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+
 import classes from "./HeaderCartButton.module.css";
 import CardIcon from "../Cart/CartIcon";
+import CardContext from "../../store/cart-context";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CardContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CardIcon></CardIcon>
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
-          3
-      </span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import classes from './Card.module.css'
 
 const Card = (props)=>{
